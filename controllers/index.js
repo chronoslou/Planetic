8ac498710b9a3b60c192fd3c50245faf20ce0828
Lou Falla <lou3892@gmail.com>
2021-05-17 22:39:26 -0400

    fixed homeroutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
-const htmlRoutes = require("./homeroutes");
+const htmlRoutes = require("./homeRoutes");
 
 router.use("/api", apiRoutes);
 router.use("/", htmlRoutes);
